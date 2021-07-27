af5b36be3a35a59e5a59bcf7eb5759ce76d84fcb
Julie Ng <me@julie.io>
2021-07-27 17:43:29 +0200

    nuxt-config: server

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,5 +37,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  server: {
+    port:  process.env.PORT, // default: 3000
+    host: '0.0.0.0' // default: localhost
   }
 }
